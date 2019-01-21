f9f5e2fa5fd5df236e8be2d567ea169113886136
AlexandreAppfel <46886323+AlexandreAppfel@users.noreply.github.com>
2019-01-21 11:30:47 +0100

    Update afficher_chemin.m

--- afficher_chemin.m
+++ afficher_chemin.m
@@ -1,4 +1,4 @@
-function afficher_chemin(chemin, liste_villes)
+function afficher_chemin(chemin, liste_villes, test)
     n = length(chemin);
     villes_chemin = zeros(n+1,2);
     for i=1:n
@@ -7,5 +7,9 @@ function afficher_chemin(chemin, liste_villes)
     villes_chemin(end,:) = villes_chemin(1,:);
     figure;
     plot(villes_chemin(:,1), villes_chemin(:,2));
+    if test==1
+        title('Algorithme de Glouton pour le PVC')
+    else
+        title('Algorithme du recuit simulé pour le PVC')
         
 end

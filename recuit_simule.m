ecb9d9e5a62354110c24cae5de4f673acf5ab372
Pikbo <costehippolyte@yahoo.fr>
2019-01-21 09:58:45 +0100

    avancée partielle sur le recuit simulé

---
+++ recuit_simule.m
@@ -0,0 +1,12 @@
+function optimum = recuit_simule(nbr_ville,N_iter, alpha, T0)
+    X0 = randperm(nbr_ville);
+    X_trajectoire = zeros(1,N_iter-1);
+    X_trajectoire(1) = X0;
+    for i = 2:N_iter
+        
+    
+    
+    
+    
+
+end

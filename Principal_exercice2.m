81ebfa890a45ef02537a2a41a07eaf43504b3abe
Pikbo <costehippolyte@yahoo.fr>
2019-01-28 08:34:07 +0100

    fonction qui marche

---
+++ Principal_exercice2.m
@@ -0,0 +1,12 @@
+% [X,Y] = meshgrid(-50:.5:50);                                
+% Z = schaffer(X,Y);
+
+[X,Y] = meshgrid(-10:.2:10);
+
+A = sin(X).*cos(Y).*abs(100 - sqrt(X.^2 + Y.^2)/pi);
+Z = -0.0001*(abs(A)+1);
+% Z = X.*exp(-X.^2-Y.^2);
+
+surf(X,Y,Z);
+
+
--- schaffer.m
+++ schaffer.m
@@ -1,6 +1,6 @@
 function [y] = schaffer(x1,x2)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-y = 0.5 + (cos(sin(abs(x1^2 - x2^2))) - 0.5)/(1 + 0.001*(x1^2 + x2^2))^2 
+y = 0.5 + (cos(sin(abs(x1.^2 - x2.^2))) - 0.5)./(1 + 0.001*(x1.^2 + x2.^2)).^2; 
 end
 

ab5773adf4e6b2f2ac6b4634db5d1daa0637de18
Pikbo <costehippolyte@yahoo.fr>
2019-01-28 10:25:56 +0100

    pso qui marche

--- PSO.m
+++ PSO.m
@@ -1,46 +1,42 @@
-function [outputArg1,outputArg2] = PSO(omega,alpha,beta,xmin,xmax,ymin,ymax,f,itermax)
+function [Z_global,X_global] = PSO(omega,alpha,beta,xmin,xmax,ymin,ymax,itermax,N_part)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-x = xmin + (xmax-xmin).*rand;
-y = ymin + (ymax - ymin).*rand;
+X = xmin + (xmax-xmin).*rand(2,N_part);
+VX = zeros(2,N_part);
 
-vx = 0;
-vy = 0;
+X_local = X;
+X_global = X(:,1);
 
-xopt = x;
-yopt = y;
-zopt = f(x,y);
+for i=1:N_part
+    if schaffer(X(1,i),X(2,i)) < schaffer(X_global(1),X_global(2))
+        Xglobal = X(:,i);
+    end
+end
+
 
 for k=1:itermax;
     r1 = rand;
     r2 = rand;
     
-    vx_courant = omega*vx + alpha*r1*(xopt - x) + beta*r2*(zopt - x);
-    vy_courant = omega*vy + alpha*r1*(xopt - y) + beta*r2*(zopt - y);
+    VX_courant = omega*VX + alpha*r1*(X_local - X) + beta*r2*(X_global - X);
+    X_courant = X + VX_courant;
     
-    x_courant = x + vx_courant;
-    y_courant = y + vy_courant;
-    
-    z = f(x_courant,y_courant);
-    if z < f(x,y)
-        x = x_courant;
-        y = y_courant;
+        
+    for m = 1:N_part
+        if schaffer(X_courant(1,m),X_courant(2,m)) < schaffer(X_local(1,m),X_local(2,m))
+            X_local(:,m) = X_courant(:,m);
+        end
     end
     
-    if z < zopt
-        xopt = x_courant;
-        yopt = y_courant;
-        zopt = z;
-        
-    
-    
+    for p= 1:N_part
+        if schaffer(X_courant(1,m),X_courant(2,m)) < schaffer(X_global(1),X_global(2))
+            X_global= X_courant(:,m);
+        end
+    end
 
+    
 end
-
-
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+Z_global = schaffer(X_global(1),X_global(2));
 end
 
--- Principal_exercice2.m
+++ Principal_exercice2.m
@@ -1,12 +1,14 @@
 % [X,Y] = meshgrid(-50:.5:50);                                
 % Z = schaffer(X,Y);
 
-[X,Y] = meshgrid(-10:.2:10);
-
-A = sin(X).*cos(Y).*abs(100 - sqrt(X.^2 + Y.^2)/pi);
-Z = -0.0001*(abs(A)+1);
+% [X,Y] = meshgrid(-10:.2:10);
+% 
+% A = sin(X).*cos(Y).*abs(100 - sqrt(X.^2 + Y.^2)/pi);
+% Z = -0.0001*(abs(A)+1);
 % Z = X.*exp(-X.^2-Y.^2);
+% 
+% surf(X,Y,Z);
 
-surf(X,Y,Z);
+PSO(0.5,0.5,1,-5,5,-5,5,5000,10)
 
 

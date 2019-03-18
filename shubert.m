013dbef6cd99e0b7fd6ecd74da5dc4f9e431d0fe
AlexandreAppfel <46886323+AlexandreAppfel@users.noreply.github.com>
2019-03-18 09:46:14 +0100

    Add files via upload

--- shubert.m
+++ shubert.m
@@ -1,6 +1,6 @@
-function Z = shubert(x,y)
-%     I = length(X);
-%     J = length(Y);
+function [Z] = shubert(x,y)
+%     I = length(x);
+%     J = length(y);
 %     Z = zeros(I,J);
 %     for i=1:I
 %         for j=1:J
@@ -10,7 +10,7 @@ function Z = shubert(x,y)
                 s_x = s_x + k*cos((k+1)*x + k);
                 s_y = s_y + k*cos((k+1)*y + k);
             end
-            Z = s_x*s_y;
+            Z = s_x.*s_y;
 %         end
 %     end      
 end
